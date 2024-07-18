310b2fca176c6a82442fecef09e6a930bc39101b
Jon Cantero <jcantero1998@gmail.com>
2024-07-18 15:00:15 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hotelbediax-destinations' title`, () => {
+  it(`should have the 'hotelbediax' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hotelbediax-destinations');
+    expect(app.title).toEqual('hotelbediax');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hotelbediax-destinations');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hotelbediax');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
+    children: [
+      {
+        path: 'destinations',
+        title: 'Destinations',
+        loadComponent: () => import('./dashboard/pages/destinations/destinations.component').then(c => c.DestinationsComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'destinations',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo:  '/dashboard',
+    pathMatch: 'full',
+  },
+];
