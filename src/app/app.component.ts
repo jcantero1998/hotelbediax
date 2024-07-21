f38844a37649038ce87e325569633f08ed53465d
Jon Cantero <jcantero1998@gmail.com>
2024-07-21 10:51:17 +0200

    added footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ToolbarComponent, SidebarComponent],
+  imports: [ToolbarComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
